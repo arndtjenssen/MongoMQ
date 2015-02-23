93416890580c306aee53e860714d66e848cd3b25
Arndt Jenssen <arndt.jenssen@gmailc.om>
2015-02-24 00:38:14 +0100

    testing index.js

---
+++ index.js
@@ -0,0 +1,6 @@
+exports.MongoMQ = require('./lib/MongoMQ');
+exports.Eventer = require('./lib/eventer');
+exports.Logger = require('./lib/logger');
+exports.MongoConnection = require('./lib/MongoConnection');
+exports.options = require('./lib/options');
+exports.QueueMonitor = require('./lib/QueueMonitor');
